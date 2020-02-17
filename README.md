0e30136fb8c80198a5c1dfa8c64496d2dc6d3a68
Hendrik <hendrik.poulsen-nautrup@uibk.ac.at>
2020-02-17 02:54:17 +0100

    tutorial
    first version

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-rl-ion-trap-tutorial
+# Tutorial: Reinforcement Learning in Ion Traps
+
+This tutorial teaches you to build your own physics-inspired reinforcement 
+learning (RL) environment featuring simulations of a few ions manipulated by 
+laser pulses.
+
+## Get Started
+
+1.  Build the RL environment interactively in the IPython notebook 
+    *Ion Trap Environment*.
+2.  Check out how a simple tabular projective simulation agent works under the 
+    hood by looking at the fully commented, short code in *ps.py*
+3.  Try it out yourself and let an agent learn in your newly built environment
+    by following the instructions in the IPython notebook 
+    *Reinforcement Learning for Ion Traps*.
+4.  Explore what else you can do with this environment when using 
+    different agents, maybe even deep RL.
+5.  Find more ingenious ways of translating physics problems into RL 
+    environments.
+
+## Preliminaries
+
+Before we get to the meat, here is some preliminary information. 
+`numpy` and `scipy` are the most important python extensions which we are going 
+to rely on in this tutorial. 
+Some jupyter notebook extensions are useful. So if you don't have any 
+extensions, install them as described 
+[here](https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html) 
+and [here](https://github.com/Jupyter-contrib/jupyter_nbextensions_configurator):
+
+```bash
+pip install --user jupyter_contrib_nbextensions
+pip install --user jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable --user
+```
+
+This allows you to activate certain packages that may come in handy. 
+Specifically for the exercise *Ion Trap Environment* `Collapsible headings` is 
+a plus as well as `Freeze`. 
+Strictly speaking however you don't need any. They are just for the experience.
---
+++ ps.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+class PSAgent(object):
+    def __init__(self, num_actions, glow=0.1, damp=0., softmax=0.1):
+        """
+        Simple, 2-layered projective simulation (PS) agent.
+
+        Args:
+            num_actions (int): The number of available actions.
+            glow (float, optional): The glow (or eta) parameter. Defaults to 0.1
+            damp (float, optional): The damping (or gamma) parameter. 
+                                    Defaults to 0.
+            softmax (float, optional): The softmax (or beta) parameter. 
+                                       Defaults to 0.1.                
+
+        NOTE: This simple version misses some features such as clip deletion
+              or emotion tags.
+        """
+        self.num_actions = num_actions
+        self.glow = glow
+        self.damp = damp
+        self.softmax = softmax
+        #int: current number of percepts.
+        self.num_percepts = 0
+        #np.ndarray: h-matrix with current h-values. Defaults to all 1.
+        self.hmatrix = np.ones([1,self.num_actions])
+        #np.ndarray: g-matrix with current glow values. Defaults to all 0.
+        self.gmatrix = np.zeros([1,self.num_actions])
+        #dict: Dictionary of percepts as {"percept": index}
+        self.percepts={}
+        
+    def predict(self, observation):
+        """
+        Given an observation, returns an action.
+
+        (1) Create a percept from an observation.
+        (2) Add percept if it has not been encountered before.
+        (3) Get action from h-values.
+        (4) Update g-matrix.
+
+        Args:
+            percept (object): A percept in form of an object.
+        
+        Returns:
+            action (int): The action to be performed.
+        """
+        # (1) create percept from observation
+        percept = self._get_percept(observation)
+        # (2) add percept to clip network if it has not been encountered before
+        if percept not in self.percepts.keys():
+            # add new percept
+            self.percepts[percept] = self.num_percepts
+            # increment number of percepts
+            self.num_percepts += 1
+            # add column to h-matrix
+            self.hmatrix = np.append(self.hmatrix, 
+                                     np.ones([1,self.num_actions]),
+                                     axis=0)
+            # add column to g-matrix
+            self.gmatrix = np.append(self.gmatrix, 
+                                     np.zeros([1,self.num_actions]),
+                                     axis=0)
+        
+        # (3) get action from h-value
+        # get index from dictionary entry
+        percept_index = self.percepts[percept]
+        # get h-values
+        h_values = self.hmatrix[percept_index]
+        # get probabilities from h-values through a softmax function
+        prob = self._softmax(h_values)
+        # get action
+        action = np.random.choice(range(self.num_actions), p=prob)
+        
+        # (4) update g-matrix
+        self.gmatrix[int(percept_index),int(action)] = 1
+
+        return action
+
+    def train(self, reward):
+        """
+        Given a reward, updates h-matrix. 
+        Updates g-matrix with glow.
+
+        Args:
+            reward (float): The received reward.
+        """
+        # damping h-matrix
+        self.hmatrix=self.hmatrix - self.damp*(self.hmatrix-1.)
+        # update h-matrix
+        self.hmatrix+=reward*self.gmatrix
+        # update g-matrix
+        self.gmatrix=(1-self.glow)*self.gmatrix
+    
+    # ----------------- helper methods -----------------------------------------
+
+    def _get_percept(self, observation):
+        """
+        Given an observation, returns a percept.
+        This function is just to emphasize the difference between observations
+        issued by the environment and percepts, which describes the observation
+        as perceived by the agent.
+
+        Args:
+            observation (object): The observation in some form of encoding.
+
+        Returns:
+            percept (str): The observation encoded as a percept.
+        """
+        percept = str(observation)
+        return percept
+    
+    def _softmax(self, x):
+        """
+        Given an input, calculates the normalized exponential function.
+
+        Args:
+            x (np.ndarray): The input array.
+        
+        Returns:
+            softmax_x (np.ndarray): The softmax distribution over the input.
+        """
+        # rescale exponential to avoid large numbers
+        rescale = max(x)
+        exp_x = np.exp(self.softmax*(x-rescale))
+        # get normalization
+        norm = sum(exp_x)
+        # calculate normalized exponential
+        softmax_x = exp_x/norm
+
+        return softmax_x
